0292bab888ff759c91feef1cea50c5872143f69d
fahad ahmed Bakhshwain <info@fontbold.top>
2025-08-17 15:51:27 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: "/artist-landing2025/"   // مهم يطابق اسم الريبو
-});
+  base: '/artist-landing2025/',   // <— مهم
+})
